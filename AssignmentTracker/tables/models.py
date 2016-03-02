88037003ff61cf01f3595d9e5c76123fd2b8c38d
jiahao.cao <jiahao.cao@outlook.com>
2016-03-02 14:26:56 -0700

    add .gitignore, modify setting, model, admin, create a superuser

--- AssignmentTracker/tables/models.py
+++ AssignmentTracker/tables/models.py
@@ -7,11 +7,17 @@ class User(models.Model):
     username = models.CharField(max_length=18)
     password = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.username
+
 class Course(models.Model):
     username = models.CharField(max_length=18)
     course = models.CharField(max_length=40)
     difficulty = models.IntegerField(default=1)
     url = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.course
 
 
 class Assignments(models.Model):
@@ -20,3 +26,6 @@ class Assignments(models.Model):
     start = models.CharField(max_length=10)
     end = models.CharField(max_length=10)
     difficulty = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.assignment
