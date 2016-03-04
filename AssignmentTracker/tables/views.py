4af385191f453a86f9d9fd68c275aa2c3fcd5da1
NatJC <jiahao.cao@outlook.com>
2016-03-04 13:33:25 -0700

    add a test mainpage

--- AssignmentTracker/tables/views.py
+++ AssignmentTracker/tables/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import User, Course, Assignments
 
 # Create your views here.
+def test_main(request):
+    user = User.objects.all()
+    course = Course.objects.all()
+    assigns = Assignments.objects.all()
+    return render(request, 'tables/test_main.html', {'user': user, 'course': course, 'assigns': assigns})
