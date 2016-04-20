ac76ebd6745c00a485dff1ce4afa84199b6c9696
Bendroste <bedr5044@colorado.edu>
2016-04-19 20:11:02 -0600

    added indvidual database calls per table

--- AssignmentTracker/scripts/sqlite_call.py
+++ AssignmentTracker/scripts/sqlite_call.py
@@ -1,14 +1,173 @@
 import sys
+rt sys
 import sqlite3
 
 
-def db_select(table, where):
+def db_select_user(where):
+
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+
+query = "select * from tables_user where" + where 
+
+#call stored Procedure
+c.execute(query)
+r = c.fetchall()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return (r)
+
+def db_insert_user(values):
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+query = "insert into tables_user values" + values
+#call stored Procedure
+c.execute(query)
+conn.commit()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return
+
+def db_delete_user(where):
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+query = "delete from tables_user where " + where
+#call stored Procedure
+c.execute("delete from tables_user where username = 'root1'")
+conn.commit()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return
+
+
+
+def db_select_course(where):
+
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+
+query = "select * from tables_course where" + where 
+
+#call stored Procedure
+c.execute(query)
+r = c.fetchall()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return (r)
+
+def db_insert_course(values):
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+query = "insert into tables_course values" + values
+#call stored Procedure
+c.execute(query)
+conn.commit()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return
+
+def db_delete_course(where):
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+query = "delete from tables_course where " + where
+#call stored Procedure
+c.execute(query)
+conn.commit()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return
+
+
+def db_select_assignments(where):
+
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+
+query = "select * from tables_assignments where" + where 
+
+#call stored Procedure
+c.execute(query)
+r = c.fetchall()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return (r)
+
+def db_insert_assignments(values):
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+query = "insert into tables_assignments values" + values
+#call stored Procedure
+c.execute(query)
+conn.commit()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return
+
+def db_delete_assignments(where):
+#connect to DB
+conn = sqlite3.connect('../db.sqlite3')
+c = conn.cursor()
+query = "delete from tables_assignments where " + where
+#call stored Procedure
+c.execute(query)
+conn.commit()
+#Do what is needed to get results to UI
+#TODO Figure out what UI needs to make use of results
+#for no print for testing
+
+#close connection
+conn.close()
+return
+import sqlite3
+
+
+def db_select_user(where):
 
 	#connect to DB
 	conn = sqlite3.connect('../db.sqlite3')
 	c = conn.cursor()
 	
-	query = "select * from " + table + where 
+	query = "select * from tables_user where" + where 
 	
 	#call stored Procedure
 	c.execute(query)
@@ -21,11 +180,11 @@ def db_select(table, where):
 	conn.close()
 	return (r)
 	
-def db_insert(table, values):
+def db_insert_user(values):
 	#connect to DB
 	conn = sqlite3.connect('../db.sqlite3')
 	c = conn.cursor()
-	query = "insert into " + table + " values" + values
+	query = "insert into tables_user values" + values
 	#call stored Procedure
 	c.execute(query)
 	conn.commit()
@@ -37,11 +196,11 @@ def db_insert(table, values):
 	conn.close()
 	return
 	
-def db_delete(table, where):
+def db_delete_user(where):
 	#connect to DB
 	conn = sqlite3.connect('../db.sqlite3')
 	c = conn.cursor()
-	query = "delete from " + table + " where " + where
+	query = "delete from tables_user where " + where
 	#call stored Procedure
 	c.execute("delete from tables_user where username = 'root1'")
 	conn.commit()
@@ -52,9 +211,108 @@ def db_delete(table, where):
 	#close connection
 	conn.close()
 	return
+	
+	
+	
+def db_select_course(where):
 
-#TODO#
-#Create Log File
-#update This code to send results to UI
-#create stored proc
-#findout connection to DB
+	#connect to DB
+	conn = sqlite3.connect('../db.sqlite3')
+	c = conn.cursor()
+	
+	query = "select * from tables_course where" + where 
+	
+	#call stored Procedure
+	c.execute(query)
+	r = c.fetchall()
+	#Do what is needed to get results to UI
+	#TODO Figure out what UI needs to make use of results
+	#for no print for testing
+	
+	#close connection
+	conn.close()
+	return (r)
+	
+def db_insert_course(values):
+	#connect to DB
+	conn = sqlite3.connect('../db.sqlite3')
+	c = conn.cursor()
+	query = "insert into tables_course values" + values
+	#call stored Procedure
+	c.execute(query)
+	conn.commit()
+	#Do what is needed to get results to UI
+	#TODO Figure out what UI needs to make use of results
+	#for no print for testing
+	
+	#close connection
+	conn.close()
+	return
+	
+def db_delete_course(where):
+	#connect to DB
+	conn = sqlite3.connect('../db.sqlite3')
+	c = conn.cursor()
+	query = "delete from tables_course where " + where
+	#call stored Procedure
+	c.execute(query)
+	conn.commit()
+	#Do what is needed to get results to UI
+	#TODO Figure out what UI needs to make use of results
+	#for no print for testing
+	
+	#close connection
+	conn.close()
+	return
+	
+	
+def db_select_assignments(where):
+
+	#connect to DB
+	conn = sqlite3.connect('../db.sqlite3')
+	c = conn.cursor()
+	
+	query = "select * from tables_assignments where" + where 
+	
+	#call stored Procedure
+	c.execute(query)
+	r = c.fetchall()
+	#Do what is needed to get results to UI
+	#TODO Figure out what UI needs to make use of results
+	#for no print for testing
+	
+	#close connection
+	conn.close()
+	return (r)
+	
+def db_insert_assignments(values):
+	#connect to DB
+	conn = sqlite3.connect('../db.sqlite3')
+	c = conn.cursor()
+	query = "insert into tables_assignments values" + values
+	#call stored Procedure
+	c.execute(query)
+	conn.commit()
+	#Do what is needed to get results to UI
+	#TODO Figure out what UI needs to make use of results
+	#for no print for testing
+	
+	#close connection
+	conn.close()
+	return
+	
+def db_delete_assignments(where):
+	#connect to DB
+	conn = sqlite3.connect('../db.sqlite3')
+	c = conn.cursor()
+	query = "delete from tables_assignments where " + where
+	#call stored Procedure
+	c.execute(query)
+	conn.commit()
+	#Do what is needed to get results to UI
+	#TODO Figure out what UI needs to make use of results
+	#for no print for testing
+	
+	#close connection
+	conn.close()
+	return
--- AssignmentTracker/scripts/sqlite_call_test.py
+++ AssignmentTracker/scripts/sqlite_call_test.py
@@ -1,55 +1,44 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Andy Sayler
-# Summer 2014
-# CSCI 3308
-# Univerity of Colorado
-# Text Processing Module
+# -*- coding:utf-8 -*-
 
 import unittest
 import sqlite_call
 
-class TextprocTestCase(unittest.TestCase):
+@classmethod
+def tearDownClass(cls):
+pass
 
-	@classmethod
-	def setUpClass(cls):
-		pass
+def setUp(self):
+pass
 
-	@classmethod
-	def tearDownClass(cls):
-		pass
+def tearDown(self):
+pass
 
-	def setUp(self):
-		pass
+def test_select(self):
+sqlite_call.db_insert_user("(112,'root1', 'test1')")
+p = sqlite_call.db_select_user(" username = 'root1'")
+t = p[0]
+sqlite_call.db_delete_user(" username = 'root1'")
+self.assertEqual(t[1], "root1", "did not select correctly")
 
-	def tearDown(self):
-		pass
+def test_insert(self):
+p = sqlite_call.db_select_user(" username = 'root1'")
+sqlite_call.db_insert_user("(112,'root1', 'test1')")
+t = sqlite_call.db_select_user(" username = 'root1'")
+sqlite_call.db_delete_user("username = 'root1'")
+self.assertNotEqual(p, t, "did not insert to DB")
 
-	def test_select(self):
-		sqlite_call.db_insert("tables_user(username, password)","('root1', 'test1')")
-		p = sqlite_call.db_select('tables_user'," where username like 'root1'")
-		t = p[0]
-		sqlite_call.db_delete("tables_user", "username = 'root1'")
-		self.assertEqual(t[1], "root1", "did not select correctly")
-	
-	def test_insert(self):
-		p = sqlite_call.db_select('tables_user'," where username like 'root1'")
-		sqlite_call.db_insert("tables_user(username, password)","('root1', 'test1')")
-		t = sqlite_call.db_select('tables_user'," where username like 'root1'")
-		sqlite_call.db_delete("tables_user", "username = 'root1'")
-		self.assertNotEqual(p, t, "did not insert to DB")
-		
-	def test_Delete(self):
-		sqlite_call.db_insert("tables_user(username, password)","('root1', 'test1')")
-		p = sqlite_call.db_select('tables_user'," where username like 'root1'")
-		sqlite_call.db_delete("tables_user", "username = 'root1'")
-		t = sqlite_call.db_select('tables_user'," where username like 'root1'")
-		self.assertNotEqual(p, t, "did not Delete to DB")
-		
+def test_Delete(self):
+sqlite_call.db_insert_user("(112,'root1', 'test1')")
+p = sqlite_call.db_select_user(" username like 'root1'")
+sqlite_call.db_delete_user("username = 'root1'")
+t = sqlite_call.db_select_user(" username like 'root1'")
+self.assertNotEqual(p, t, "did not Delete to DB")
+
 # Add Your Test Cases Here..
 
 # Main: Run Test Cases
 if __name__ == '__main__':
     unittest.main()
+
 
